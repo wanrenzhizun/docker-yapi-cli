2524562978ebfdc2cf483d1b1015b37b684cf9ff
wanrenzhizun <31756438+wanrenzhizun@users.noreply.github.com>
2022-11-12 10:18:00 +0800

    Update install_plugins.js

--- install_plugins.js
+++ install_plugins.js
@@ -27,20 +27,5 @@ if (packages.length > 0){
         console.log(`子进程退出码：${code}`);
     });
 }
-if (fs.existsSync(`/yapi/init.lock`)){
-    console.log("尝试启动Yapi")
-    require('/yapi/vendors/server/app.js');
-}else {
-    console.log("启动安装程序。。。");
-    require('/yapi/vendors/handle.js');
-    child_process.exec("npm run install-server", (error, stdout, stderr) => {
-        if (!error) {
-            console.log(`${stdout}`);
-            console.log(`${stderr}`);
-        } else {
-            console.log(`${error}`);
-        }
-    });
-}
 
 
